f5c3b8c73fcdcd991093b35e0237ed458301bf21
Liam <liam@enuffsaid.co.nz>
2021-04-21 19:47:09 +1200

    Added name check

--- app/src/main/groovy/restProject/App.groovy
+++ app/src/main/groovy/restProject/App.groovy
@@ -2,6 +2,7 @@ package restProject
 
 import io.restassured.RestAssured
 import io.restassured.http.Method
+import io.restassured.path.json.JsonPath
 import io.restassured.response.Response
 import io.restassured.specification.RequestSpecification
 
@@ -15,6 +16,9 @@ class App {
         RestAssured.baseURI = "http://swapi.dev/api"
         RequestSpecification httpRequest = RestAssured.given()
         Response response = httpRequest.request(Method.GET, "/planets/1/")
+        JsonPath jsonPathEvaluator = response.jsonPath()
+        String name = jsonPathEvaluator.get("name")
+        assert name == "Tatooine"
         String responseBody = response.getBody().asString()
         int statusCode = response.getStatusCode()
         assert statusCode == 200
